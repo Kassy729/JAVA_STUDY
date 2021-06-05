d5d668f6a7c7db34bd91b9681ca0064d101360ed
Kassy729 <79403903+Kassy729@users.noreply.github.com>
2021-06-05 22:46:17 +0900

    CoptFile

---
+++ CopyFile1.java
@@ -0,0 +1,31 @@
+package Network;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyFile1 {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		FileInputStream in = null;  //파일에서 바이트를 읽는다.
+		FileOutputStream out = null;  //파일에서바이트를 쓴다.
+		
+		try {
+			in = new FileInputStream("input.txt");
+			out = new FileOutputStream("output.txt");
+			int c;
+			
+			while ((c = in.read()) != -1) {
+				out.write(c);
+			}
+			System.out.println(c);
+		} finally {
+			if(in != null)
+				in.close();
+			if(out != null)
+				out.close();
+		}
+	}
+
+}
